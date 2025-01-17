0773b9a5fddc932c2e7865a07316d6ddc88fe3b0
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-17 18:15:21 +0530

    Initial commit

---
+++ aws-resource-list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will list all the resources in the AWS account
+# Author : Parthasarathy G
+# Version: v0.0.1
+
+# Following are the supported AWS services by the script
+# 1. EC2
+# 2. S3
+# 3. RDS
+# 4. DynamoDB
+# 5. Lambda
+# 6. IAM
+# 7. CloudFormation
+# 8. CloudWatch
+# 9. VPC
+# 10. SNS
