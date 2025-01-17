0a634b19da5bad314c486b3a0bbac4a51727d702
Parthasarathy-G <parthasarathy0580@gmail.com>
2025-01-17 18:33:48 +0530

    Shell file added

--- aws-resource-list.sh
+++ aws-resource-list.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#################################################################
+
 # This script will list all the resources in the AWS account
 # Author : Parthasarathy G
 # Version: v0.0.1
@@ -15,3 +17,24 @@
 # 8. CloudWatch
 # 9. VPC
 # 10. SNS
+
+#Usage: ./aws-resource-list.sh <region> <service-name>
+#Example: ./aws-resource-list.sh us-east-1 ec2
+
+#################################################################
+
+# Check if the required arguments are passed
+
+   if [ $# -ne 2 ]; then
+    echo "Usage: $0 <region> <service-name>"
+    exit 1
+fi
+
+#Check if the AWS CLI is installed
+
+if ! command -v aws &> /dev/null
+then
+    echo "AWS CLI could not be found. Please install the AWS CLI"
+    exit
+fi
+
